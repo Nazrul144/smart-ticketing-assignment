bd9e70120f47cbec6c443484eb223e64850406d4
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-02-19 23:09:20 +0600

    project updated

--- scrtpt/script.js
+++ scrtpt/script.js
@@ -3,16 +3,19 @@
 //Increase the current taka by 550 Taka
 //Show the updated taka
 
-let sub = 1;
+let sub = 1;let selectedSeat = 0;
 function TitcketCalculation(btn){
+    if(selectedSeat >= 4) {
+        return;
+    }
+    selectedSeat++;
     //Reducing Seats 
     const totalSeatsSelector = document.getElementById('total-seats');
     const currentSeats = +totalSeatsSelector.innerText; 
     let availableSeats = currentSeats - sub;
     totalSeatsSelector.innerHTML = availableSeats;
     btn.style.backgroundColor = "#1DD100";
-    
-
+    btn.disabled = true;
 
     const selectedBtn = (btn.id);
     const selectedContainer = document.getElementById("creatingUl");
@@ -55,14 +58,9 @@ function sumOfTicket(){
     let totalCost = initialTotalValue + perTicketPrice;
     totalTicketPrice.innerHTML = totalCost;
 
-
     //Updating Grand Total
     const GrandTotal = document.getElementById("grand-total");
-    const initialGrandTotal = +GrandTotal.innerText;
-    let totalGrandTotal = initialGrandTotal + perTicketPrice;
-    GrandTotal.innerHTML = totalGrandTotal;
-    
-   
+    GrandTotal.innerHTML = totalCost;
 }
 
 //Cupupon Matching
@@ -80,7 +78,19 @@ if(input === ""){
     alert("Provide a Coupon!");
 }
 else if(secrateCupuponCode.includes(input)){
-
+   
+    let totalTicketPrice = +document.getElementById("total-cost").innerText;
+    let couponCode = document.getElementById("inputValue").value;
+    let discountPercentage = 0;
+    if(couponCode === "NEW15"){
+        discountPercentage = 15;
+    }
+    else if(couponCode === "Couple 20"){
+        discountPercentage = 20;
+    }
+    let totalDiscountAmount = (totalTicketPrice * discountPercentage) / 100;
+    const GrandTotalSelector = document.getElementById("grand-total");
+    GrandTotalSelector.innerHTML = totalTicketPrice - totalDiscountAmount;
     document.getElementById("inputValue").style.display = "none";
     applyBtn.style.display = "none";
 }
@@ -92,25 +102,10 @@ else{
 })
 
 
+//Popup function
 
-//Button Color:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function closePopup(){
+    document.getElementById("popup");
+    console.log("clicked");
+}
 
